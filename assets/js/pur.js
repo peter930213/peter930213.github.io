c31d17d1853a83e886e6ebc4ad4fdcae76b25255
peter930213 <149244650+peter930213@users.noreply.github.com>
2024-01-08 00:15:07 +0800

    bloodmud.github.io
    bloodmud.github.io

---
+++ assets/js/pur.js
@@ -0,0 +1,58 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+
+let object;
+
+const loader = new GLTFLoader();
+
+loader.load(
+    `models/eye/website.fbx`,
+    function (gltf) {
+        object = gltf.scene;
+        scene.add(object);
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+        console.error(error);
+    }
+);
+
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById("container3D").appendChild(renderer.domElement);
+camera.position.z = 25; // 因為只有 "eye" 資料夾，可以直接設定 z 位置
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    if (object) {
+        object.rotation.y = -3 + mouseX / window.innerWidth * 3;
+        object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
+    }
+
+    renderer.render(scene, camera);
+}
+
+window.addEventListener("resize", function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+document.onmousemove = function (e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+};
+
+animate();
+
+
+
